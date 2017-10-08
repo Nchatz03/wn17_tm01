3fa080095d532efcbbc4af307d87c2f20db2f283
Nchatz03 <chatzigiannisnick@gmail.com>
2017-10-08 19:05:01 +0300

    Modification Commit
    MultiThreadedTCPServer.java
    - add command line arguments
    ---args[0] = servers listening port
    ---args[1] = servers request limitation number
    -Bug fixing

--- MultiThreadedTCPServer.java
+++ MultiThreadedTCPServer.java
@@ -9,43 +9,38 @@ import java.util.concurrent.Executors;
 public class MultiThreadedTCPServer {
 
 	/* max thread number for threadpool */
-	private static int maxclientnum = 10;
 
-	public static ExecutorService TCP_WORKER_SERVICE = Executors.newFixedThreadPool(maxclientnum);
+	public static ExecutorService TCP_WORKER_SERVICE = Executors.newFixedThreadPool(ServerGlobal.MAXTHREAD);
 
+	@SuppressWarnings("resource")
 	public static void main(String args[]) {
+
+		ServerGlobal.PORT = Integer.parseInt(args[0]);
+		ServerGlobal.REQUEST_LIMIT = Integer.parseInt(args[1]);
+		ServerGlobal.LIMITFLAG = false;
+
 		try {
-			
-			
 
 			//////////////////////////////////////////////////////////////////////////////////////////////
 			/* create Socket */
-			ServerSocket socket = new ServerSocket(90);
+			ServerSocket socket = new ServerSocket(ServerGlobal.PORT);
 			//////////////////////////////////////////////////////////////////////////////////////////////
-			
-			
 
 			/* Connection Establishment Message */
-			System.out.println("Server listening to: " + socket.getInetAddress() + ":" + socket.getLocalPort());
+			System.out.println("Server listening to: " + socket.getInetAddress() + ":" + ServerGlobal.PORT);
 
+			long initiallize = System.currentTimeMillis();
+			ServerGlobal.SERVER_INIT_TIME = initiallize;
 			while (true) {
-				
-				
 
 				//////////////////////////////////////////////////////////////////////////////////////////////
 				/* server accepts client */
 				Socket client = socket.accept();
 				//////////////////////////////////////////////////////////////////////////////////////////////
-				
-				
 
 				/* Submit client to a thread */
 				TCP_WORKER_SERVICE.submit(new TCPWorker(client));
-				
-				if(client.isClosed()){
-					socket.close();
-					
-				}
+				ServerGlobal.CLIENT_SUBMIT++;
 
 			}
 		} catch (IOException e) {
