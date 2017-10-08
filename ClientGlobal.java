b42f73ac078a1bc472c47c2819b126753250ae27
Nchatz03 <chatzigiannisnick@gmail.com>
2017-10-08 17:01:54 +0300

    first commit
    ClientGlobal.java
     -Global variables for client
    ServerGlobal.java
    -Global variables for server and TCPWorker

---
+++ ClientGlobal.java
@@ -0,0 +1,16 @@
+package com.tcp.client;
+
+public class ClientGlobal {
+
+	 static String INSTANCE_IP;
+	 static int PORT;
+
+	 static int TOTAL_REQUEST_COMPLETE = 0;
+	 static int CLIENTS_CONNECTED = 5;
+	 static int CLIENT_SERVED = 0;
+
+	 static long id = 1;
+
+	 static double LATENCY_TOTAL;
+
+}
---
+++ ServerGlobal.java
@@ -0,0 +1,14 @@
+package com.tcp.server;
+
+public class ServerGlobal {
+
+	static int REQUEST_LIMIT;
+	static int PORT;
+	static int CLIENT_SUBMIT = 0;
+	static int CLIENT_SERVED = 0;
+	static long SERVER_INIT_TIME;
+	static int MAXTHREAD = 10;
+	static int REQUESTS_REACHED = 0;
+	static int THROUGHPUT_MILESTONE =0;
+
+}
